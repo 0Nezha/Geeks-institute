1d99b29f4eb884cfc216e6a91fd52356fd523574
Nezha AIT <nezhaaitelhad@gmail.com>
2025-08-20 00:31:17 +0100

    Update

--- week1/day1/daily-challenges/Daily-challenge.py
+++ week1/day1/daily-challenges/Daily-challenge.py
@@ -1,16 +1,15 @@
 #Challenge1
-#Ask the user for a number and a length.
+#1.Ask the user for a number and a length.
 number = int(input("Enter a number: "))
 length = int(input("Enter a length: "))
 
-#Create a program that prints a list of multiples of the number until the list length reaches length.
+#2
 multiples = []
 for i in range(1, length + 1):
     multiples.append(number * i)
 print(multiples)
 
- #Challenge2
- #Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
+ #Challenge2 
 string = input("Enter a string: ")
 new_string = ""
 for i in range(len(string)):
