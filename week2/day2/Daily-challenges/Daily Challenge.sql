099992e3da29ccca61dfafdf62a6a6398885d44d
Nezha AIT <nezhaaitelhad@gmail.com>
2025-08-22 18:25:34 +0100

    challenge commit

---
+++ week2/day2/Daily-challenges/Daily Challenge.sql
@@ -0,0 +1,10 @@
+--Daily challenge : SQL Puzzle
+
+--Q1: la requête va afficher 0 
+
+--Q2:   La requête retourne le nombre de lignes de FirstTab dont l’id n’apparaît pas dans SecondTab. 
+-- alors va afficher 2 (id 6 et 7)
+
+--Q3: resultat : 0 
+
+--Q4:  resultat : 2 ,(6 et 7 )
