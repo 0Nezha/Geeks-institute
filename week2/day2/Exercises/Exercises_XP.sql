03f58b9dba6e012a93cea290262b0fa2e8be3da7
Nezha AIT <nezhaaitelhad@gmail.com>
2025-08-25 03:01:36 +0100

    ExerciseXP commit

--- week2/day2/Exercises/Exercises_XP.sql
+++ week2/day2/Exercises/Exercises_XP.sql
@@ -87,3 +87,4 @@ SELECT customer.customer_id, customer.first_name, customer.last_name, payment.am
 FROM customer
 JOIN payment ON customer.customer_id = payment.customer_id
 ORDER BY payment.staff_id;
+
