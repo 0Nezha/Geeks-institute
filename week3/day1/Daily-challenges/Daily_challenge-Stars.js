9329fedff5cb455df095e89a7291b018a57e38c2
Nezha AIT <nezhaaitelhad@gmail.com>
2025-09-01 01:14:06 +0100

    nnnnnnaaaaa

---
+++ week3/day1/Daily-challenges/Daily_challenge-Stars.js
@@ -0,0 +1,16 @@
+//using one loop:
+let result = ""
+for (let i = 0; i < 6; i++) {
+    result += "*";
+    console.log(result);
+}
+
+
+//using two nested for loops
+for (let i = 0; i < 6; i++) {
+    let result2 = "";
+  for (let j = 0; j <= i; j++) {
+    result2 += "*";
+  }
+  console.log(result2);
+}
